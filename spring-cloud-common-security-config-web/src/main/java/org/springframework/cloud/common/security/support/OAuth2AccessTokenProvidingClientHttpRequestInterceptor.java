041d8079bb2e12f869c13fb4cd5fff68622faa81
Gunnar Hillert <ghillert@pivotal.io>
2019-08-12 22:25:05 -1000

    gh-58 Polishing

--- spring-cloud-common-security-config-web/src/main/java/org/springframework/cloud/common/security/support/OAuth2AccessTokenProvidingClientHttpRequestInterceptor.java
+++ spring-cloud-common-security-config-web/src/main/java/org/springframework/cloud/common/security/support/OAuth2AccessTokenProvidingClientHttpRequestInterceptor.java
@@ -17,19 +17,13 @@ package org.springframework.cloud.common.security.support;
 
 import java.io.IOException;
 
-import org.springframework.cloud.common.security.ManualOAuthAuthenticationDetails;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
-import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.util.Assert;
-import org.springframework.util.StringUtils;
 
 /**
  * This implementation of a {@link ClientHttpRequestInterceptor} will retrieve, if available, the OAuth2 Access Token
@@ -55,33 +49,8 @@ public class OAuth2AccessTokenProvidingClientHttpRequestInterceptor implements C
 	@Override
 	public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution)
 			throws IOException {
-		final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-		final String token;
-
-		if (StringUtils.hasText(this.staticOauthAccessToken)) {
-			token = this.staticOauthAccessToken;
-		}
-		else if (authentication != null && authentication instanceof OAuth2Authentication) {
-			final OAuth2Authentication auth2Authentication = (OAuth2Authentication) authentication;
-
-			if (auth2Authentication.getDetails() instanceof OAuth2AuthenticationDetails) {
-				final OAuth2AuthenticationDetails auth2AuthenticationDetails =
-					(OAuth2AuthenticationDetails) auth2Authentication.getDetails();
-				token = auth2AuthenticationDetails.getTokenValue();
-			}
-			else if (auth2Authentication.getDetails() instanceof ManualOAuthAuthenticationDetails) {
-				ManualOAuthAuthenticationDetails manualOAuthAuthenticationDetails =
-					(ManualOAuthAuthenticationDetails) auth2Authentication.getDetails();
-				token = manualOAuthAuthenticationDetails.getAccessToken().getValue();
-			}
-			else {
-				token = null;
-			}
-		}
-		else {
-			token = null;
-		}
+		final String token = TokenUtils.getAccessToken();
 
 		if (token != null) {
 			request.getHeaders().add(HttpHeaders.AUTHORIZATION, OAuth2AccessToken.BEARER_TYPE + " " + token);
